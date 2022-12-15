907902616704cdff236b5d6e06dfedd515a9cd43
LuizGustavoDuarteChagas <elfzero0@gmail.com>
2022-12-15 15:13:23 -0300

    Exercicio 4 e arquivo do exercicio 6

--- ListaDeExercicios0-Matrizes/Lista0Exercicio04.java
+++ ListaDeExercicios0-Matrizes/Lista0Exercicio04.java
@@ -1,8 +1,38 @@
 //Nome: Luiz Gustavo Duarte Chagas
 
 import java.util.Scanner;
-public class Lista0Exercicio02 {
+public class Lista0Exercicio04 {
 	public static void main (String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int n, m;
+
+		do{
+			System.out.println("Digite a quantidade de linhas");
+			n = ler.nextInt();
+		} while(n < 1);
 		
+		do{
+			System.out.println("Digite a quantidade de colunas");
+			m = ler.nextInt();
+		} while(m < 1);
+		
+
+		String[][] A = new String[n][m];
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				A[i][j] = String.valueOf((int) (Math.random() * 100));
+			}
+		}
+		System.out.println("\n A matriz B gerada e:");
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				System.out.print("\t" + A[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
---
+++ ListaDeExercicios0-Matrizes/Lista0Exercicio06.java
@@ -0,0 +1,10 @@
+//Nome: Luiz Gustavo Duarte Chagas
+
+import java.util.Scanner;
+
+public class Lista0Exercicio06 {
+
+	public static void main (String[] args) {
+		
+	}
+}
