5e7ba26425e014128b624095a8f42c81f775933c
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-01-12 16:11:17 -0300

    Lista 4 Exercicio 1

---
+++ ListaDeExercicios4Pt1-OrientacaoAObjetos/Lampada.java
@@ -0,0 +1,24 @@
+public class Lampada{
+	private String marca, modelo, tipo;
+	private boolean estado;
+	public Lampada(){
+	}
+	public void setMarca{
+		marca = sMarca;
+	}
+	public void setModelo{
+		modelo = sModelo;
+	}
+	public void setTipo{
+		tipo = sTipo;
+	}
+	public String getMarca{
+		return marca;
+	}
+	public String getModelo{
+		return modelo;
+	}
+	public String getTipo{
+		return tipo;
+	}
+}
