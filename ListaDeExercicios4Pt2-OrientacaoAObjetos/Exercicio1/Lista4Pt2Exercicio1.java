e3509bd47be790b01f763c6ee5fd2144d1cb51f6
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-01-14 19:14:27 -0300

    Adicionando Lista 4 Parte 2 Exercicio 1

--- ListaDeExercicios4Pt2-OrientacaoAObjetos/Exercicio1/Lista4Pt2Exercicio1.java
+++ ListaDeExercicios4Pt2-OrientacaoAObjetos/Exercicio1/Lista4Pt2Exercicio1.java
@@ -6,36 +6,14 @@ entrada um valor lógico, que quando verdadeiro, incrementa em um a idade da pes
 método deve ser chamado por fazAniversario, não havendo retorno. Siga o diagrama abaixo para
 implementar a classe Pessoa. Após crie uma classe para testar. Observe os sinais de mais e de
 menos antes dos nomes dos atributos e métodos. Respeite-os*/
-public class Pessoa{
-	private String nome;
-	private int idade;
-	private double altura;
-	public Pessoa(){
-		this.nome = "";
-		this.idade = 0;
-		this.altura = 0;
-	}
-	public void setNome(String nome){
-		this.nome = nome;
-	}
-	public void setIdade(int idade){
-		this.idade = idade;
-	}
-	public void setAltura(double altura){
-		this.altura = altura;
-	}
-	public String getNome(){
-		return nome;
-	}
-	public int getIdade(){
-		return idade;
-	}
-	public double getAltura(){
-		return altura;
-	}
-	public void fazAniversario(boolean fazAniversario){
-		if(fazAniversario){
-			idade++;
-		}
+public class Lista4Pt2Exercicio1{
+	public static void main(String[] args){
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome("Lucas");
+		pessoa.setIdade(18);
+		pessoa.setAltura(1.79);
+		System.out.print("Nome: " + pessoa.getNome() + "\nIdade (antes do aniversario): " + pessoa.getIdade());
+		pessoa.fazAniversario(true);
+		System.out.print("\nIdade (depois do aniversario): " + pessoa.getIdade() + "Altura: " + pessoa.getAltura());
 	}
 }
