7198c08e1624242837c153eac18a82de090982bf
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-01-18 20:18:22 -0300

    Renomeando arquivos lista 4

---
+++ ListaDeExercicios4Pt1-OrientacaoAObjetos/Exercicio7/ModeloDeComputador.java
@@ -0,0 +1,11 @@
+/*
+7) Escrever uma classe ModeloDeComputador que encapsule valores que definam a configuração de um
+microcomputador (tipo de processador, memória RAM, tamanho do disco rígido, tamanho do monitor, por
+exemplo). Essa classe deve ter um método calcularPreco que calculará o preço do computador como sendo
+a soma do custo de seus componentes:
+• Placa-mãe: R$800
+• Opções de processadores: 1600Mhz a R$700, 1800Mhz a R$830, 1900Mhz a R$910
+• Opções de memória: 1GB, 2GB, 4GB, 6GB ou 8GB, cada 1GB custa R$350.
+• Opções de disco rígido: 500 GB a R$300, 1 TB a R$420, 2TB a R$500.
+• Opções de monitor: 15
+polegadas a R$320, 17 polegadas a R$520
