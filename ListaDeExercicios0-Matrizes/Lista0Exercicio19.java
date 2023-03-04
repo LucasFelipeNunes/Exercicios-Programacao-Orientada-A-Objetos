150d068af5b8d0b8f36170b3112cb64a9431bb87
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-03-04 11:05:57 -0300

    Lista 0 Exercicio 19

--- ListaDeExercicios0-Matrizes/Lista0Exercicio19.java
+++ ListaDeExercicios0-Matrizes/Lista0Exercicio19.java
@@ -1,9 +1,23 @@
 /*Nome: Lucas Felipe da Silva Nunes
 Enunciado: 19) Escreva um programa em Java para gerar uma matriz quadrada S de dimensão N.
-Imprimir os elementos da “asa da borboleta”.*/
+Imprimir os elementos da "asa da borboleta".*/
 import java.util.Scanner;
 public class Lista0Exercicio19{
 	public static void main (String args[]){
 		Scanner ler = new Scanner(System.in);
+        System.out.print("Digite a dimensão N da matriz: ");
+        int N = ler.nextInt();
+        int[][] S = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                S[i][j] = (int) (Math.random() * 10);
+                if (i == j || i == N - j - 1 || j == 0 || j == N - 1) {
+                    System.out.print(S[i][j] + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
 	}
 }
