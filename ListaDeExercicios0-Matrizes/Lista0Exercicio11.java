0ba22bf22d5f997612ddf9b7642cdd9613526de5
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-03-24 15:26:21 -0300

    Update Lista0Exercicio11.java

--- ListaDeExercicios0-Matrizes/Lista0Exercicio11.java
+++ ListaDeExercicios0-Matrizes/Lista0Exercicio11.java
@@ -1,29 +1,29 @@
 /*Nome: Lucas Felipe da Silva Nunes
-11) Escreva um programa em Java
-para gerar uma matriz quadrada K de dimensão N.
-Imprimir os elementos da abaixo da diagonal principal (inclusa).*/
+Enunciado: 11) Escreva um programa em Java para gerar uma matriz quadrada K de dimensão N. Imprimir os elementos da abaixo da diagonal principal (inclusa).*/
+
 import java.util.Scanner;
+
 public class Lista0Exercicio11{
 	public static void main (String[] args){
 		Scanner ler = new Scanner(System.in);
-		double N = 0;
+		int N = 0;
 		do{
 			System.out.print("Digite a dimensao inteira e positiva N da matriz quadrada K:\t");
-			N = ler.nextDouble();
-			if((N*10)%10 != 0 || N <= 0){
+			N = ler.nextInt();
+			if(N <= 0){
 				System.out.print("Valor invalido. ");
 			}
-		}while((N*10)%10 != 0 || N <= 0);
-		double[][] K = new double[(int)N][(int)N];
+		}while(N <= 0);
+		double[][] K = new double[N][N];
 		System.out.print("Elementos abaixo da diagonal principal:");
-		for (int i = 0; i < N; i++)
+		for (int i = 0; i < K.length; i++)
 		{
-			for (int j = 0; j < N; j++)
+			for (int j = 0; j < K[i].length; j++)
 			{
-				K[i][j] = Math.random() * 20;
+				K[i][j] = Math.random() * 50;
 			}
 		}	
-		for (int i = 0; i < N - 1; i++)
+		for (int i = 0; i + 1 < K[i].length; i++)
 		{
 			System.out.print("\t" + K[i + 1][i]);
 		}
