7d7e849f760e51009784f89a10e2f7f1a6862fab
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-03-24 15:31:15 -0300

    Update Lista0Exercicio13.java

--- ListaDeExercicios0-Matrizes/Lista0Exercicio13.java
+++ ListaDeExercicios0-Matrizes/Lista0Exercicio13.java
@@ -1,22 +1,25 @@
 /*Nome: Lucas Felipe da Silva Nunes
-Enunciado: 13) Escreva um programa em Java para gerar uma matriz quadrada K de dimensão N.
-Imprimir os elementos da abaixo da diagonal secundária (exclusa)*/
+Enunciado: 13) Escreva um programa em Java para gerar uma matriz quadrada K de dimensão N. Imprimir os elementos da abaixo da diagonal secundária (exclusa)*/
+
 import java.util.Scanner;
+
 public class Lista0Exercicio13{
 	public static void main (String args[]){
 		Scanner ler = new Scanner(System.in);
-		int N = 0;
+		int dimensaoN = 0;
 		do{
 			System.out.print("Digite a dimensao N da matriz K:\t");
-			N = ler.nextInt();
-			if(N <= 0) System.out.print("Valor invalido. ");
-		}while(N <= 0);
-		int[][] K = new int[N][N];
+			dimnesaoN = ler.nextInt();
+			if(dimensaoN <= 0){
+				System.out.print("Valor invalido. ");
+			}
+		}while(dimensaoN <= 0);
+		int[][] K = new int[dimensaoN][dimensaoN];
 		for (int i = 0; i < K.length; i++)
 		{
-			for (int j = 0; j < K.length; j++)
+			for (int j = 0; j < K[i].length; j++)
 			{
-				System.out.print("Digite o valor da posicao [" + i + "][" + j + "] da matriz K:\t");
+				System.out.print("Digite o valor da posicao [" + i + "][" + j + "] da matriz K: ");
 				K[i][j] = ler.nextInt();
 			}
 		}
