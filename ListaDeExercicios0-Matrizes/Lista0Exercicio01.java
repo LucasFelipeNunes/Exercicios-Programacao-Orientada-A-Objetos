28496f4771d631e2e0c9c87a06814317fbc78438
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-03-24 14:56:54 -0300

    Update Lista0Exercicio01.java

--- ListaDeExercicios0-Matrizes/Lista0Exercicio01.java
+++ ListaDeExercicios0-Matrizes/Lista0Exercicio01.java
@@ -2,23 +2,25 @@
 1) Escreva um programa em Java para
 ler uma matriz A de 4 linhas por 5 colunas e
 imprimir seus elementos.*/
+
 import java.util.Scanner;
+
 public class Lista0Exercicio01{
 	public static void main (String[] args) {
 		Scanner ler = new Scanner(System.in);
 		String[][] A = new String[4][5];
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < A.length; i++)
 		{
-			for (int j = 0; j < 5; j++)
+			for (int j = 0; j < A[i].length; j++)
 			{
-				System.out.print("Digite o elemento da linha " + (i + 1) + " e da coluna " + (j + 1) + " da matriz A:\t");
+				System.out.print("Digite o elemento da posicapo [" + (i + 1) + "][" + (j + 1) + "] da matriz A:\t");
 				A[i][j] = ler.nextLine();
 			}
 		}
-		System.out.println("\n A matriz A digitada é:");
-		for (int i = 0; i < 4; i++)
+		System.out.println("\nMatriz A:");
+		for (int i = 0; i < A.length; i++)
 		{
-			for (int j = 0; j < 5; j++)
+			for (int j = 0; j < A[i].length; j++)
 			{
 				System.out.print("\t" + A[i][j]);
 			}
