76ba6f679580eafbf6c8d5a4b2ae2e3d1e36e967
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-03-24 15:10:55 -0300

    Update Lista0Exercicio07.java

--- ListaDeExercicios0-Matrizes/Lista0Exercicio07.java
+++ ListaDeExercicios0-Matrizes/Lista0Exercicio07.java
@@ -1,33 +1,33 @@
 /*Nome: Lucas Felipe da Silva Nunes
-Enunciado: 7) Escreva um programa em Java para gerar uma matriz E de dimensão N x M, deter-
-minar e imprimir o maior e o menor elemento deste conjunto, informando a posição
-dos mesmos.*/
+Enunciado: 7) Escreva um programa em Java para gerar uma matriz E de dimensão N x M, determinar e imprimir o maior e o menor elemento deste conjunto, informando a posição dos mesmos.*/
+
 import java.util.Scanner;
+
 public class Lista0Exercicio07{
 	public static void main (String[] args) {
 		Scanner ler = new Scanner(System.in);
-		double N = 0, M = 0, menorValor = 0, maiorValor = 0;
+		int N = 0, M = 0, menorValor = 0, maiorValor = 0;
 		String posicaoMenorValor = "", posicaoMaiorValor = ""; 
 		do{
 			System.out.print("Digite a dimensao inteira e positiva N de elementos na linha da matriz a ser digitada:\t");
-			N = ler.nextDouble(); 
-			if((N*10)%10 != 0 || N <= 0){
+			N = ler.nextInt(); 
+			if(N <= 0){
 				System.out.print("Valor invalido. ");
 			}
-		}while((N*10)%10 != 0 || N <= 0);
+		}while(N <= 0);
 		do{
 			System.out.print("Digite a dimensao inteira e positiva M de elementos na coluna da matriz a ser digitada:\t");
-			M = ler.nextDouble();
-			if((M*10)%10 != 0 || M <= 0){
+			M = ler.nextInt();
+			if(M <= 0){
 				System.out.print("Valor invalido. ");
 			}
-		}while((M*10)%10 != 0 || M <= 0);
+		}while(M <= 0);
 		System.out.println("Vetor E gerado:\t");
-		double[][] E = new double[(int)N][(int)M];
-		for (int i = 0; i < N; i++){
+		double[][] E = new double[N][M];
+		for (int i = 0; i < E.length; i++){
 			System.out.println();
-			for (int j = 0; j < M; j++){
-				E[i][j] = Math.random() * 20;
+			for (int j = 0; j < E[i].length; j++){
+				E[i][j] = Math.random() * 50;
 				System.out.print("\t" + E[i][j]);
 				if(menorValor > E[i][j] || (j == 0 && i == 0)){
 					menorValor = E[i][j];
