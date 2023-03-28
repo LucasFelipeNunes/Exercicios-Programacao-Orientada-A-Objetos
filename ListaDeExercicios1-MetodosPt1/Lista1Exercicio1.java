4de8f4aa5f2ea93dca93a034c9cb37eba665c415
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-03-28 19:29:21 -0300

    Update Lista1Exercicio1.java

--- ListaDeExercicios1-MetodosPt1/Lista1Exercicio1.java
+++ ListaDeExercicios1-MetodosPt1/Lista1Exercicio1.java
@@ -3,14 +3,20 @@ Enunciado: 1) [POO-001] Crie programa que solicite ao usuário um número tipo d
 positivo ou não. A verificação deve ser feita através de um método que receba um valor (parâmetro de
 entrada) e informe se ele é positivo ou negativo através de um retorno do tipo boolean. Assuma o número
 zero como positivo. Declare como: boolean isPositivo(double num)*/
+
 import java.util.Scanner;
+
 public class Lista1Exercicio1{
 	public static void main (String args[]){
+	
 		Scanner ler = new Scanner(System.in);
-		System.out.print("Digite um numero:\t");
+		
+		System.out.print("Digite um numero: ");
+		
 		System.out.print("O numero digitado e " + (isPositivo(ler.nextDouble()) ? "positivo." : "negativo."));
 	}
-	static boolean isPositivo(double num){
-		return num >= 0;
+	
+	static boolean isPositivo(double numero){
+		return numero >= 0;
 	}
 }
