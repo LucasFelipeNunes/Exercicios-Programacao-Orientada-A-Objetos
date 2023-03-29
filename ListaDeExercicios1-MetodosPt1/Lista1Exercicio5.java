5bae880b2b95e6d030e0bfea21f963598fa0f567
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-03-28 22:33:18 -0300

    Update Lista1Exercicio5.java

--- ListaDeExercicios1-MetodosPt1/Lista1Exercicio5.java
+++ ListaDeExercicios1-MetodosPt1/Lista1Exercicio5.java
@@ -1,15 +1,7 @@
 /*Nome: Lucas Felipe da Silva Nunes
 Enunciado: 5) [POO-005] Baseado no exercício anterior, crie uma nova versão, que calcula as raízes de uma equação do
 segundo grau: ax² + bx + c = 0. Para ela existir, o coeficiente 'a' deve ser diferente de zero. Caso o delta seja
-
-
-
-
-
-maior ou igual a zero, as raízes serão reais. Caso o delta seja neg
-ativo, as reais serão complexas
-e da forma
-,
+maior ou igual a zero, as raízes serão reais. Caso o delta seja negativo, as reais serão complexas e da forma,
 assim retorne zero.*/
 import java.util.Scanner;
 
@@ -23,41 +15,39 @@ public class Lista1Exercicio5 {
         do {
             System.out.print("a: ");
             a = scan.nextDouble();
+            
             if(a == 0) {
                 System.out.print("Valor invalido");
             }
+        
         } while (a == 0); 
         
         System.out.print("b: ");
         double b = ler.nextDouble();
 
-        System.out.println("c: ");
+        System.out.print("c: ");
         double c = ler.nextDouble();
 
         double delta = calcularDelta(a, b, c);
-        System.out.println("O delta e: " + delta + ( (delta < 0) ? ", de raiz imaginaria" : ", de raiz real" ) );
+        System.out.println("Delta e: " + delta + ", e tem raiz " + ( (delta < 0) ? "imaginaria." : "real." ) );
 
-        double[] raizes = calcularBhaskara(a, b, c);
-
-        System.out.print("As raizes sao: " + raizes[0] + ", " + raizes[1]);
-
+        double[] raizes = calcularRaizes(a, b, c);
+        System.out.print("As raizes sao " + raizes[0] + " e " + raizes[1]);
     }
 
     static double calcularDelta(double a, double b, double c) {
-        return Math.pow(b, 2) - 4*a*c;
+        return Math.pow(b, 2) - 4 * a * c;
     }
 
-    static double[] calcularBhaskara(double a, double b, double c) {
+    static double[] calcularRaizes(double a, double b, double c) {
 
         double[] raizes = new double[2];
 
-        if(calcularDelta(a, b, c) < 0) {
-            return raizes;
-        } else {
-            raizes[0] = (-b + Math.sqrt(calcularDelta(a, b, c))) / 2*a;
-            raizes[1] = (-b - Math.sqrt(calcularDelta(a, b, c))) / 2*a;
-
-            return raizes;
+        if(calcularDelta(a, b, c) >= 0) {
+           raizes[0] = (-b + Math.sqrt(calcularDelta(a, b, c))) / 2 * a;
+           raizes[1] = (-b - Math.sqrt(calcularDelta(a, b, c))) / 2 * a;
         }
+        
+        return raizes;
     }
 }
