36623b61b9274e5caa79102ed7dfdbede108c70b
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-03-28 22:44:47 -0300

    Update Lista1Exercicio2.java

--- ListaDeExercicios1-MetodosPt1/Lista1Exercicio2.java
+++ ListaDeExercicios1-MetodosPt1/Lista1Exercicio2.java
@@ -1,12 +1,26 @@
-/*Nome: Lucas Felipe da Silva Nunes*/
+/*Nome: Lucas Felipe da Silva Nunes
+Enunciado: 2) [POO-002] Crie programa que solicite ao usuário um número tipo double. Após verifique se o número é 
+zero ou não. A verificação deve ser feita através de um método que receba um valor (parâmetro de 
+entrada) e informe se ele é zero ou não através de um retorno do tipo boolean. Declare como: boolean 
+isZero(double num).
+Entrada:                        Saída:
+Entre com um número: -1,48      O número -1.48 não é zero.
+Entre com um número: 3,14       O número 3.14 não é zero.
+Entre com um número: 0          O número 0.0 é zero.*/
+
 import java.util.Scanner;
+
 public class Lista1Exercicio2 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Digite um numero: ");
-        double numero = scan.nextDouble();
-       System.out.println(isZero(numero) ? numero + ", e zero" : numero + ", Nao e zero");
+        
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.print("Entre com um numero: ");
+        double numero = ler.nextDouble();
+        
+        System.out.print("O numero " + numero + (isZero(numero) ? "" : "nao") + "e zero.");
     }
+    
     static boolean isZero(double numero) {
         return numero == 0;
     }
