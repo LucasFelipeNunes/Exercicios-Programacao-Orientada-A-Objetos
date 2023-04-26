be3265701169f930a23cbb9fe521ac8a7f53d1f1
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-04-26 18:05:47 -0300

    Update Lista4Pt1Exercicio7.java

--- ListaDeExercicios4Pt1-OrientacaoAObjetos/Exercicio7/Lista4Pt1Exercicio7.java
+++ ListaDeExercicios4Pt1-OrientacaoAObjetos/Exercicio7/Lista4Pt1Exercicio7.java
@@ -12,25 +12,27 @@ a soma do custo de seus componentes:
 
 public class Lista4Pt1Exercicio7{
   public static void main(String[] args{
-    int frequenciaProcessador = 0, armazenamentoMemoria = 0, armazenamentoDiscoRigido = 0, tamanhoMonitor = 0;
+    int freqProcessador = 0, armMemoria = 0, armDiscoRigido = 0, tamanhoMonitor = 0;
     ModeloDeComputador modelo = new ModeloDeComputador();
     System.out.print("Monte um computador e veja o preco\n\nPlaca-mae: R$800,00\nFrequencias de processador disponiveis:\n- 1600Mhz: R$700,00\n- 1800Mhz: R$830,00\n- 1900Mhz: R$910,00\n");
     do{
       System.out.print("Digite o valor da frequencia que voce deseja: ");
-      int frequenciaProcessador = ler.nextInt();
-      if(frequenciaProcessador != 1600 && frequenciaProcessador != 1800 && frequenciaProcessador != 1900){
+      freqProcessador = ler.nextInt();
+      modelo.setProcessador((freqProcessador == 1600) ? 700 : (freqProcessador == 1800) ? 830 : 910);
+      if(freqProcessador != 1600 && freqProcessador != 1800 && freqProcessador != 1900){
         System.out.print("Valor invalido. ");
       }
-    }while(frequenciaProcessador != 1600 && frequenciaProcessador != 1800 && frequenciaProcessador != 1900);
+    }while(freqProcessador != 1600 && freqProcessador != 1800 && freqProcessador != 1900);
     System.out.print("\nOpcoes de Memoria: 1GB, 2GB, 4GB, 6GB ou 8GB\nCada GB custa R$350,00\n\n");
     
     do{
       System.out.print("Digite a quantidade de memoria: ");
-      int frequenciaProcessador = ler.nextInt();
-      if(frequenciaProcessador != 1600 && frequenciaProcessador != 1800 && frequenciaProcessador != 1900){
+      int armMemoria = ler.nextInt();
+      modelo.setMemoria(armMemoria * 350);
+      if(armMemoria != 1600 && armMemoria != 1800 && armMemoria != 1900){
         System.out.print("Valor invalido. ");
       }
-    }while(frequenciaProcessador != 1600 && frequenciaProcessador != 1800 && frequenciaProcessador != 1900);
+    }while(armMemoria != 1600 && armMemoria != 1800 && armMemoria != 1900);
     
   }
 }
