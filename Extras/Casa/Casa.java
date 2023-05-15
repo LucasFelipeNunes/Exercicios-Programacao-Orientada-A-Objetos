6387b7983db63eba4b4d6218a4c7631016a970a3
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2023-05-15 17:53:29 -0300

    Update Casa.java

--- Extras/Casa/Casa.java
+++ Extras/Casa/Casa.java
@@ -1,30 +1,64 @@
 public class Casa{
 	private String cor;
 	private boolean portas[];
+	private int qtdPortas;
+	
+	public Casa(){
+		setCasa(3, "branca");
+	}
 	
 	public Casa(int qtdPortas, String cor){
+		setCasa(qtdPortas, cor);
+	}
+	
+	private void setCasa(int qtdPortas, String cor){
 		this.cor = cor;
-		portas = new boolean[qtdPortas];
+		setQtdPortas(qtdPortas);
+		portas = new boolean[this.qtdPortas];
+	}
+	
+	private void setQtdPortas(int qtdPortas){
+		if(qtdPortas >= 1 && qtdPortas <= 100){
+			this.qtdPortas = qtdPortas;
+		} else {
+			this.qtdPortas = 3;
+		}
+	}
+	
+	private int getQtdPortas(){
+		return qtdPortas;
+	}
+	
+	public boolean setPorta(boolean acao, int numeroPorta){
+		numeroPorta--;
+		if(porta >= 0 && porta < 100 && portas[numeroPorta] != acao){
+			portas[numeroPorta] = acao;
+			return true;
+		}
+		return false;
 	}
 	
 	public boolean abrirPorta(int numeroPorta){
-		if(portas[numeroPorta] == false){
-			portas[numeroPorta] = true;
-			return true;
-		}
-		return false;
+		return setPorta(numeroPorta, true);
 	}
 	
 	public boolean fecharPorta(int numeroPorta){
-		if(portas[numeroPorta] == true){
-			portas[numeroPorta] = false;
-			return true;
-		}
-		return false;
+		return setPorta(numeroPorta, true);
 	}
 	
 	public boolean getPorta(int numeroPorta){
-		return portas[numeroPorta];
+		if(numeroPorta >= 0 && porta < 100){
+			return portas[numeroPorta];
+		}
+		return false; //eu sei que ta errado...
+	}
+	
+	public int qtdPortasAbertas(){
+		soma = 0;
+		for(int atual = 0;atual < qtdPortas;atual++){
+			soma += (portas[atual] ? 1 : 0);
+		}
+		return soma;
 	}
 	
 	public boolean trocarCor(String cor){
@@ -33,5 +67,14 @@ public class Casa{
 		}
 		this.cor = cor;
 		return true;
+	}
+	
+	@Override
+	public String toString(){
+		String saida = "";
+		
+		for(int atual = 0;atual < qtdPortas;atual++){
+			saida += "\nPorta" + (atual + 1) + ": " + (getPorta(atual) ? "aberta\n" : "fechada\n");
+		}
 	}
 }
